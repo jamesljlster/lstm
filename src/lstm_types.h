2e14c5a82d4785b2a0e1171aeddedc90eba7301a
Zheng-Ling Lai <jamesljlster@gmail.com>
2017-08-12 18:25:16 +0800

    Add lstm types

---
+++ src/lstm_types.h
@@ -0,0 +1,24 @@
+#ifndef __LSTM_TYPES_H__
+#define __LSTM_TYPES_H__
+
+struct LSTM_BASE
+{
+	double* weight;		// Weight connection
+	double* wGrad;		// Gradient summation of weight
+	double* wDelta;		// Momentum of weight
+
+	double* rWeight;	// Recurrent weight connection
+	double* rGrad;		// Gradient summation of recurrent weight
+	double* rDelta;		// Momentum of recurrent weight
+
+	double th;			// Threshold of the node
+	double thGrad;		// Gradient summation of threshold
+	double thDelta;		// Momentum of threshold
+};
+
+struct LSTM_NODE
+{
+
+};
+
+#endif
