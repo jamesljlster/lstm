0238d8be8e5199e63d29a6efbe91d9ec3598d521
Zheng-Ling Lai <jamesljlster@gmail.com>
2017-08-17 17:39:53 +0800

    Finish lstm_alloc_network()

--- src/lstm_alloc.c
+++ src/lstm_alloc.c
@@ -3,8 +3,117 @@
 #include <assert.h>
 
 #include "lstm_private.h"
+#include "lstm_builtin_math.h"
 
 #include "debug.h"
+
+int lstm_alloc_network(struct LSTM_STRUCT* lstm)
+{
+	int i;
+	int ret = LSTM_NO_ERROR;
+	int nodeType, reNetSize;
+
+	int layers;
+	int* nodeList;
+
+	struct LSTM_LAYER* layerRef = NULL;
+	struct LSTM_CONFIG_STRUCT* cfgRef;
+
+	LOG("enter");
+
+	// Set reference
+	cfgRef = &lstm->config;
+	layers = cfgRef->layers;
+	nodeList = cfgRef->nodeList;
+
+	// Checking
+	if(layers < 3 || nodeList == NULL)
+	{
+		ret = LSTM_INVALID_ARG;
+		goto RET;
+	}
+
+	// Allocate layer list
+	lstm_alloc(layerRef, layers, struct LSTM_LAYER, ret, RET);
+	for(i = 0; i < cfgRef->layers; i++)
+	{
+		// Set node type and recurrent network size
+		if(i == 0)
+		{
+			nodeType = LSTM_INPUT_NODE;
+			reNetSize = 0;
+		}
+		else if(i == 1)
+		{
+			nodeType = LSTM_FULL_NODE;
+			reNetSize = nodeList[layers - 1];
+		}
+		else if(i == layers - 1)
+		{
+			nodeType = LSTM_OUTPUT_NODE;
+			reNetSize = 0;
+		}
+		else
+		{
+			nodeType = LSTM_FULL_NODE;
+			reNetSize = 0;
+		}
+
+		// Allocate layer struct
+		ret = lstm_layer_alloc(&layerRef[i], nodeList[i], nodeType, nodeList[i], reNetSize);
+		if(ret != LSTM_NO_ERROR)
+		{
+			goto ERR;
+		}
+
+		// Set node count
+		layerRef[i].nodeCount = nodeList[i];
+
+		// Set activation function
+		if(cfgRef->inputTFunc < 0 || cfgRef->inputTFunc >= LSTM_TFUNC_AMOUNT)
+		{
+			ret = LSTM_INVALID_ARG;
+			goto ERR;
+		}
+		else
+		{
+			layerRef[i].inputTFunc = lstm_transfer_list[cfgRef->inputTFunc];
+			layerRef[i].inputDTFunc = lstm_transfer_derivative_list[cfgRef->inputTFunc];
+		}
+
+		if(cfgRef->outputTFunc < 0 || cfgRef->outputTFunc >= LSTM_TFUNC_AMOUNT)
+		{
+			ret = LSTM_INVALID_ARG;
+			goto ERR;
+		}
+		else
+		{
+			layerRef[i].outputTFunc = lstm_transfer_list[cfgRef->outputTFunc];
+			layerRef[i].outputDTFunc = lstm_transfer_derivative_list[cfgRef->outputTFunc];
+		}
+
+		if(cfgRef->gateTFunc < 0 || cfgRef->gateTFunc >= LSTM_TFUNC_AMOUNT)
+		{
+			ret = LSTM_INVALID_ARG;
+			goto ERR;
+		}
+		else
+		{
+			layerRef[i].gateTFunc = lstm_transfer_list[cfgRef->gateTFunc];
+			layerRef[i].gateDTFunc = lstm_transfer_derivative_list[cfgRef->gateTFunc];
+		}
+	}
+
+ERR:
+	for(i = 0; i < cfgRef->layers; i++)
+	{
+		lstm_layer_delete(&layerRef[i]);
+	}
+
+RET:
+	LOG("exit");
+	return ret;
+}
 
 int lstm_base_alloc(struct LSTM_BASE* basePtr, int netSize, int reNetSize)
 {
@@ -129,7 +238,6 @@ int lstm_layer_alloc(struct LSTM_LAYER* layerPtr, int nodeCount, int nodeType, i
 	}
 
 	// Assign value
-	tmpLayer.nodeCount = nodeCount;
 	*layerPtr = tmpLayer;
 	goto RET;
 
