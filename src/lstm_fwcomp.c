41b87dc73ef5164a953d5443b7a89ad20f61448e
Zheng-Ling Lai <jamesljlster@gmail.com>
2017-08-17 21:40:26 +0800

    Finish lstm_forward_computation()

--- src/lstm_fwcomp.c
+++ src/lstm_fwcomp.c
@@ -7,7 +7,7 @@ void rnn_forward_computation(lstm_t lstm, double* input, double* output)
 {
 	int i, j, k;
 	int indexTmp;
-	double calcTmp;
+	double calcTmp, ig, fg, og;
 
 	struct LSTM_CONFIG_STRUCT* cfgRef = &lstm->config;
 	struct LSTM_LAYER* layerRef = lstm->layerList;
@@ -21,78 +21,77 @@ void rnn_forward_computation(lstm_t lstm, double* input, double* output)
 	}
 
 	// Backup recurrent output
-	indexTmp = cfgRef->layers - 2;
+	indexTmp = cfgRef->layers - 2; // Set indexTmp to last hidden layer
 	for(i = 0; i < layerRef[indexTmp].nodeCount; i++)
 	{
 		layerRef[indexTmp].nodeList[i].rHold = layerRef[indexTmp].nodeList[i].output;
 	}
 
-#define __rnn_fwcomp_base(baseName, tfuncName)                                 \
-	for(j = 0; j < layerRef[i].nodeCount; j++)                                 \
-	{                                                                          \
-		calcTmp = 0;                                                           \
-                                                                               \
-		if(i == 1)                                                             \
-		{                                                                      \
-			for(k = 0; k < layerRef[indexTmp].nodeCount; k++)                  \
-			{                                                                  \
-				calcTmp += layerRef[indexTmp].nodeList[k].rHold *              \
-					layerRef[1].nodeList[j].baseName.rWeight[k];               \
-			}                                                                  \
-		}                                                                      \
-                                                                               \
-		for(k = 0; k < layerRef[i - 1].nodeCount; k++)                         \
-		{                                                                      \
-			calcTmp += layerRef[i - 1].nodeList[k].output *                    \
-				layerRef[i].nodeList[j].baseName.weight[k];                    \
-		}                                                                      \
-                                                                               \
-		calcTmp += layerRef[i].nodeList[j].baseName.th;                        \
-                                                                               \
-		layerRef[i].nodeList[j].baseName.calc = calcTmp;                       \
-		layerRef[i].nodeList[j].baseName.out = layerRef[i].tfuncName(calcTmp); \
+#define __rnn_fwcomp_base(layerIndex, baseName, tfuncName) \
+	for(j = 0; j < layerRef[layerIndex].nodeCount; j++) \
+	{ \
+		calcTmp = 0; \
+		if(layerIndex == 1) \
+		{ \
+			for(k = 0; k < layerRef[indexTmp].nodeCount; k++) \
+			{ \
+				calcTmp += layerRef[indexTmp].nodeList[k].rHold * \
+					layerRef[1].nodeList[j].baseName.rWeight[k]; \
+			} \
+		} \
+		for(k = 0; k < layerRef[layerIndex - 1].nodeCount; k++) \
+		{ \
+			calcTmp += layerRef[layerIndex - 1].nodeList[k].output * \
+				layerRef[layerIndex].nodeList[j].baseName.weight[k]; \
+		} \
+		calcTmp += layerRef[layerIndex].nodeList[j].baseName.th; \
+		layerRef[layerIndex].nodeList[j].baseName.calc = calcTmp; \
+		layerRef[layerIndex].nodeList[j].baseName.out = \
+			layerRef[layerIndex].tfuncName(calcTmp); \
 	}
 
-	// Calculation
-	for(i = 1; i < cfgRef->layers; i++)
+	// Hidden layer calculation
+	for(i = 1; i < cfgRef->layers - 1; i++)
 	{
 		// Processing base network
-		__rnn_fwcomp_base(ogNet, gateTFunc);
-		__rnn_fwcomp_base(fgNet, gateTFunc);
-		__rnn_fwcomp_base(igNet, gateTFunc);
-		__rnn_fwcomp_base(inputNet, inputTFunc);
+		__rnn_fwcomp_base(i, ogNet, gateTFunc);
+		__rnn_fwcomp_base(i, fgNet, gateTFunc);
+		__rnn_fwcomp_base(i, igNet, gateTFunc);
+		__rnn_fwcomp_base(i, inputNet, inputTFunc);
 
-		/*
-		// Calculate input network
+		// Find layer outputs
 		for(j = 0; j < layerRef[i].nodeCount; j++)
 		{
-			calcTmp = 0;
+			// Copy gate values
+			ig = layerRef[i].nodeList[j].igNet.out;
+			fg = layerRef[i].nodeList[j].fgNet.out;
+			og = layerRef[i].nodeList[j].ogNet.out;
 
-			// Recurrent factor
-			if(i == 1)
-			{
-				for(k = 0; k < layerRef[indexTmp].nodeCount; k++)
-				{
-					calcTmp += layerRef[indexTmp].nodeList[k].rHold *
-						layerRef[1].nodeList[j].inputNet.rWeight[k];
-				}
-			}
+			// Find cell value
+			layerRef[i].nodeList[j].cell = fg * layerRef[i].nodeList[j].cell +
+				ig * layerRef[i].nodeList[j].inputNet.out;
 
-			// Common factor
-			for(k = 0; k < layerRef[i - 1].nodeCount; k++)
-			{
-				calcTmp += layerRef[i - 1].nodeList[k].output *
-					layerRef[i].nodeList[j].inputNet.weight[k];
-			}
-
-			// Threshold
-			calcTmp += layerRef[i].nodeList[j].inputNet.th;
-
-			layerRef[i].nodeList[j].inputNet.calc = calcTmp;
-			layerRef[i].nodeList[j].inputNet.out = layerRef[i].inputTFunc(calcTmp);
+			// Find output value
+			layerRef[i].nodeList[j].output = og *
+				layerRef[i].outputTFunc(layerRef[i].nodeList[j].cell);
 		}
-		*/
+	}
 
+	// Output layer calculation
+	indexTmp = cfgRef->layers - 1; // Set indexTmp to output layer
+	for(j = 0; j < layerRef[indexTmp].nodeCount; j++)
+	{
+		calcTmp = 0;
+		for(k = 0; k < layerRef[indexTmp - 1].nodeCount; k++)
+		{
+			calcTmp += layerRef[indexTmp - 1].nodeList[k].output *
+				layerRef[indexTmp].nodeList[j].inputNet.weight[k];
+		}
+
+		calcTmp += layerRef[indexTmp].nodeList[j].inputNet.th;
+
+		layerRef[indexTmp].nodeList[j].inputNet.calc = calcTmp;
+		layerRef[indexTmp].nodeList[j].inputNet.out = layerRef[indexTmp].outputTFunc(calcTmp);
 	}
 
 	// Get output
@@ -100,7 +99,7 @@ void rnn_forward_computation(lstm_t lstm, double* input, double* output)
 	{
 		for(i = 0; i < cfgRef->outputs; i++)
 		{
-			output[i] = layerRef[cfgRef->layers - 1].nodeList[i].output;
+			output[i] = layerRef[indexTmp].nodeList[i].inputNet.out;
 		}
 	}
 
