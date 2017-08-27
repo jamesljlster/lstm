73834bd995583e93ff3b8dfa2dd84b8c0061c70e
Zheng-Ling Lai <jamesljlster@gmail.com>
2017-08-27 15:11:45 +0800

    Add import to fwcomp test

--- test/test_fwcomp.c
+++ test/test_fwcomp.c
@@ -1,121 +1,55 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 #include <lstm.h>
-#include <lstm_private.h>
 
-#define INPUTS	2
-#define OUTPUTS	1
-#define H_LAYERS	1
-#define H_NODES		2
-
-int main()
+int main(int argc, char* argv[])
 {
-	int i, j;
+	int i;
 	int iResult;
+	int inputs, outputs;
 
 	lstm_t lstm;
 	lstm_config_t cfg;
 
-	double input[INPUTS];
-	double output[OUTPUTS];
+	double* input = NULL;
+	double* output = NULL;
 
-	// Create config
-	iResult = lstm_config_create(&cfg);
-	if(iResult < 0)
+	// Checking
+	if(argc <= 1)
 	{
-		printf("lstm_config_create() failed with error: %d\n", iResult);
+		printf("Assign a lstm model file to run the program\n");
 		return -1;
 	}
 
-	// Set config
-	lstm_config_set_inputs(cfg, INPUTS);
-	lstm_config_set_outputs(cfg, OUTPUTS);
-	lstm_config_set_hidden_layers(cfg, H_LAYERS);
-	lstm_config_set_hidden_nodes(cfg, 0, H_NODES);
-
-	// Print detail of config
-	printf("Inputs: %d\n", cfg->inputs);
-	printf("Outputs: %d\n", cfg->outputs);
-	printf("Layers: %d\n", cfg->layers);
-	printf("\n");
-	printf("Input tfunc index: %d\n", cfg->inputTFunc);
-	printf("Output tfunc index: %d\n", cfg->outputTFunc);
-	printf("Gate tfunc index: %d\n", cfg->gateTFunc);
-	printf("\n");
-	
-	for(i = 0; i < cfg->layers; i++)
+	iResult = lstm_import(&lstm, argv[1]);
+	if(iResult != LSTM_NO_ERROR)
 	{
-		printf("Nodes of %d layer: %d\n", i, cfg->nodeList[i]);
-	}
-	printf("\n");
-
-	// Create lstm
-	iResult = lstm_create(&lstm, cfg);
-	if(iResult < 0)
-	{
-		printf("lstm_create() failed with error: %d\n", iResult);
+		printf("lstm_import() failed with error: %d\n", iResult);
 		return -1;
 	}
 
-	// Set weight
-	lstm->layerList[1].nodeList[0].inputNet.weight[0] = -0.9;
-	lstm->layerList[1].nodeList[0].inputNet.weight[1] = -0.8;
-	lstm->layerList[1].nodeList[0].inputNet.rWeight[0] = -0.7;
-	lstm->layerList[1].nodeList[0].inputNet.rWeight[1] = -0.6;
-	lstm->layerList[1].nodeList[0].inputNet.th = 0.1;
+	// Get neural network configuration
+	cfg = lstm_get_config(lstm);
+	inputs = lstm_config_get_inputs(cfg);
+	outputs = lstm_config_get_outputs(cfg);
 
-	lstm->layerList[1].nodeList[0].igNet.weight[0] = -0.5;
-	lstm->layerList[1].nodeList[0].igNet.weight[1] = -0.4;
-	lstm->layerList[1].nodeList[0].igNet.rWeight[0] = -0.3;
-	lstm->layerList[1].nodeList[0].igNet.rWeight[1] = -0.2;
-	lstm->layerList[1].nodeList[0].igNet.th = 0.2;
+	//lstm_config_print(cfg);
 
-	lstm->layerList[1].nodeList[0].fgNet.weight[0] = -0.1;
-	lstm->layerList[1].nodeList[0].fgNet.weight[1] = 0.1;
-	lstm->layerList[1].nodeList[0].fgNet.rWeight[0] = 0.2;
-	lstm->layerList[1].nodeList[0].fgNet.rWeight[1] = 0.3;
-	lstm->layerList[1].nodeList[0].fgNet.th = 0.3;
+	// Memory allocation
+	input = calloc(inputs, sizeof(double));
+	output = calloc(outputs, sizeof(double));
+	if(input == NULL || output == NULL)
+	{
+		printf("Memory allocation failed\n");
+		return -1;
+	}
 
-	lstm->layerList[1].nodeList[0].ogNet.weight[0] = 0.4;
-	lstm->layerList[1].nodeList[0].ogNet.weight[1] = 0.5;
-	lstm->layerList[1].nodeList[0].ogNet.rWeight[0] = 0.6;
-	lstm->layerList[1].nodeList[0].ogNet.rWeight[1] = 0.7;
-	lstm->layerList[1].nodeList[0].ogNet.th = 0.4;
-
-	lstm->layerList[1].nodeList[1].inputNet.weight[0] = 1.0;
-	lstm->layerList[1].nodeList[1].inputNet.weight[1] = 0.9;
-	lstm->layerList[1].nodeList[1].inputNet.rWeight[0] = 0.8;
-	lstm->layerList[1].nodeList[1].inputNet.rWeight[1] = 0.7;
-	lstm->layerList[1].nodeList[1].inputNet.th = 0.5;
-
-	lstm->layerList[1].nodeList[1].igNet.weight[0] = 0.6;
-	lstm->layerList[1].nodeList[1].igNet.weight[1] = 0.5;
-	lstm->layerList[1].nodeList[1].igNet.rWeight[0] = 0.4;
-	lstm->layerList[1].nodeList[1].igNet.rWeight[1] = 0.3;
-	lstm->layerList[1].nodeList[1].igNet.th = 0.6;
-
-	lstm->layerList[1].nodeList[1].fgNet.weight[0] = 0.2;
-	lstm->layerList[1].nodeList[1].fgNet.weight[1] = 0.1;
-	lstm->layerList[1].nodeList[1].fgNet.rWeight[0] = -0.1;
-	lstm->layerList[1].nodeList[1].fgNet.rWeight[1] = -0.2;
-	lstm->layerList[1].nodeList[1].fgNet.th = 0.7;
-
-	lstm->layerList[1].nodeList[1].ogNet.weight[0] = -0.3;
-	lstm->layerList[1].nodeList[1].ogNet.weight[1] = -0.4;
-	lstm->layerList[1].nodeList[1].ogNet.rWeight[0] = -0.5;
-	lstm->layerList[1].nodeList[1].ogNet.rWeight[1] = -0.6;
-	lstm->layerList[1].nodeList[1].ogNet.th = 0.8;
-
-	lstm->layerList[2].nodeList[0].inputNet.weight[0] = 0.5;
-	lstm->layerList[2].nodeList[0].inputNet.weight[1] = 0.6;
-	lstm->layerList[2].nodeList[0].inputNet.th = 0.9;
-
-	// Forward computation
 	while(1)
 	{
-		for(i = 0; i < INPUTS; i++)
+		for(i = 0; i < inputs; i++)
 		{
-			printf("Assign %d of %d input: ", i + 1, INPUTS);
+			printf("Assign %d of %d input: ", i + 1, inputs);
 			iResult = scanf(" %lf", &input[i]);
 			if(iResult <= 0)
 			{
@@ -126,46 +60,14 @@ int main()
 
 		lstm_forward_computation(lstm, input, output);
 
-		// Print detail
-		for(i = 0; i < H_LAYERS; i++)
+		for(i = 0; i < outputs; i++)
 		{
-			printf("=== Hidden layer %d ===\n", i);
-			for(j = 0; j < H_NODES; j++)
-			{
-				printf("Hidden node %d:\n", j);
-				printf("    [Input net]\n");
-				printf("        calc: %lf\n", lstm->layerList[i + 1].nodeList[j].inputNet.calc);
-				printf("        out:  %lf\n", lstm->layerList[i + 1].nodeList[j].inputNet.out);
-				printf("\n");
-				printf("    [Input gate]\n");
-				printf("        calc: %lf\n", lstm->layerList[i + 1].nodeList[j].igNet.calc);
-				printf("        out:  %lf\n", lstm->layerList[i + 1].nodeList[j].igNet.out);
-				printf("\n");
-				printf("    [Forget gate]\n");
-				printf("        calc: %lf\n", lstm->layerList[i + 1].nodeList[j].fgNet.calc);
-				printf("        out:  %lf\n", lstm->layerList[i + 1].nodeList[j].fgNet.out);
-				printf("\n");
-				printf("    [Output gate]\n");
-				printf("        calc: %lf\n", lstm->layerList[i + 1].nodeList[j].ogNet.calc);
-				printf("        out:  %lf\n", lstm->layerList[i + 1].nodeList[j].ogNet.out);
-				printf("\n");
-				printf("    cell:   %lf\n", lstm->layerList[i + 1].nodeList[j].cell);
-				printf("    output: %lf\n", lstm->layerList[i + 1].nodeList[j].output);
-				printf("\n");
-			}
-		}
-
-		for(i = 0; i < OUTPUTS; i++)
-		{
-			printf("%d of %d output: %lf\n", i + 1, OUTPUTS, output[i]);
+			printf("%d of %d output: %lf\n", i + 1, outputs, output[i]);
 		}
 		printf("\n");
 	}
 
-	// Cleanup
-	lstm_config_delete(cfg);
 	lstm_delete(lstm);
 
 	return 0;
 }
-
