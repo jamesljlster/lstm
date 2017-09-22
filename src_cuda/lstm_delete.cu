cdd34428dead6c2199e1360b24b6b0be5c660fe7
Zheng-Ling Lai <jamesljlster@gmail.com>
2017-09-22 16:29:39 +0800

    Add lstm cuda delete module

---
+++ src_cuda/lstm_delete.cu
@@ -0,0 +1,72 @@
+#include <string.h>
+#include <cuda_runtime.h>
+
+#include "lstm_private_cuda.h"
+
+#include <debug.h>
+
+void lstm_delete_cuda(lstm_cuda_t lstmCuda)
+{
+	LOG("enter");
+
+	if(lstmCuda != NULL)
+	{
+		lstm_struct_delete_cuda(lstmCuda);
+		lstm_free(lstmCuda);
+	}
+
+	LOG("exit");
+}
+
+void lstm_struct_delete_cuda(struct LSTM_CUDA* lstmCuda)
+{
+	int i;
+
+	LOG("enter");
+
+	// Delete layers
+	if(lstmCuda->layerList != NULL)
+	{
+		for(i = 0; i < lstmCuda->config.layers; i++)
+		{
+			lstm_layer_delete_cuda(&lstmCuda->layerList[i]);
+		}
+		lstm_free(lstmCuda->layerList);
+	}
+
+	// Delete config
+	lstm_config_struct_delete(&lstmCuda->config);
+
+	// Zero memory
+	memset(lstmCuda, 0, sizeof(struct LSTM_CUDA));
+
+	LOG("exit");
+}
+
+void lstm_layer_delete_cuda(struct LSTM_CULAYER* cuLayerPtr)
+{
+	LOG("enter");
+
+	// Free device memory
+	lstm_free_cuda(cuLayerPtr->nodeMat.weight);
+	lstm_free_cuda(cuLayerPtr->nodeMat.wGrad);
+	lstm_free_cuda(cuLayerPtr->nodeMat.wDelta);
+
+	lstm_free_cuda(cuLayerPtr->nodeMat.th);
+	lstm_free_cuda(cuLayerPtr->nodeMat.thGrad);
+	lstm_free_cuda(cuLayerPtr->nodeMat.thDelta);
+
+	lstm_free_cuda(cuLayerPtr->nodeMat.calc);
+	lstm_free_cuda(cuLayerPtr->nodeMat.out);
+	lstm_free_cuda(cuLayerPtr->nodeMat.grad);
+	lstm_free_cuda(cuLayerPtr->nodeMat.gradHold);
+
+	lstm_free_cuda(cuLayerPtr->nodeMat.outQue);
+	lstm_free_cuda(cuLayerPtr->nodeMat.calcQue);
+
+	// Zero memory
+	memset(cuLayerPtr, 0, sizeof(struct LSTM_CULAYER));
+
+	LOG("exit");
+}
+
--- src_cuda/lstm_private_cuda.h
+++ src_cuda/lstm_private_cuda.h
@@ -2,7 +2,16 @@
 #define __LSTM_PRIVATE_CUDA_H__
 
 #include <lstm_private.h>
+#include "lstm_cuda.h"
 #include "lstm_types_cuda.h"
+
+// Macros
+#ifdef DEBUG
+#include <stdio.h>
+#define lstm_free_cuda(ptr)	fprintf(stderr, "%s(): free(%s), %p\n", __FUNCTION__, #ptr, ptr); cudaFree(ptr)
+#else
+#define lstm_free_cuda(ptr)	cudaFree(ptr)
+#endif
 
 #ifdef __cplusplus
 extern "C" {
@@ -10,6 +19,10 @@ extern "C" {
 
 // Private allocate function
 int lstm_layer_alloc_cuda(struct LSTM_CULAYER* cuLayerPtr, int nodeCount, int nodeType, int netSize, int reNetSize);
+
+// Private delete function
+void lstm_struct_delete_cuda(struct LSTM_CUDA* lstmCuda);
+void lstm_layer_delete_cuda(struct LSTM_CULAYER* cuLayerPtr);
 
 #ifdef __cplusplus
 }
