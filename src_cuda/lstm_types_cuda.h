00271223e4f386c885fca5969c3cd9deb0dc8f58
Zheng-Ling Lai <jamesljlster@gmail.com>
2017-09-22 15:11:19 +0800

    Update cuda types

--- src_cuda/lstm_types_cuda.h
+++ src_cuda/lstm_types_cuda.h
@@ -1,6 +1,16 @@
 #ifndef __LSTM_TYPES_CUDA_H__
 #define __LSTM_TYPES_CUDA_H__
 
+enum LSTM_CUMAT_LIST
+{
+	LSTM_CUMAT_OG,
+	LSTM_CUMAT_FG,
+	LSTM_CUMAT_IG,
+	LSTM_CUMAT_INPUT,
+
+	LSTM_CUMAT_AMOUNT
+};
+
 struct LSTM_CUMAT
 {
 	double* weight;
@@ -23,6 +33,7 @@ struct LSTM_CUMAT
 struct LSTM_CULAYER
 {
 	int nodeCount;
+	int vecLen;
 
 	struct LSTM_CUMAT nodeMat;
 };
