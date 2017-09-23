433bc33cf10170673a0d17708aee93f0ee949c87
Zheng-Ling Lai <jamesljlster@gmail.com>
2017-09-23 18:11:40 +0800

    Finish lstm_clone_to_cuda()

--- src_cuda/lstm_clone.cu
+++ src_cuda/lstm_clone.cu
@@ -9,6 +9,7 @@
 int lstm_clone_to_cuda(lstm_cuda_t* lstmCudaPtr, lstm_t lstm)
 {
 	int i, j;
+	int vecLen, nodeCount;
 	int ret = LSTM_NO_ERROR;
 
 	lstm_cuda_t tmpLstmCuda = NULL;
@@ -24,23 +25,83 @@ int lstm_clone_to_cuda(lstm_cuda_t* lstmCudaPtr, lstm_t lstm)
 	// Create lstm cuda
 	lstm_run(lstm_create_cuda(&tmpLstmCuda, cfgRef), ret, RET);
 
+#define __lstm_clone_base_to_cuda(cuMat, base) \
+	{ \
+		int procIndex = cuMat * (nodeCount * vecLen) + j * vecLen; \
+		if(cudaMemcpy(&dstLayerRef[i].nodeMat.weight[procIndex], \
+					srcLayerRef[i].nodeList[j].base.weight, \
+					srcLayerRef[i - 1].nodeCount * sizeof(double), \
+					cudaMemcpyHostToDevice) \
+				!= cudaSuccess) \
+		{ \
+			ret = LSTM_MEM_FAILED; \
+			goto ERR; \
+		} \
+		else \
+		{ \
+			procIndex += srcLayerRef[i - 1].nodeCount; \
+		} \
+ \
+		if(i == 1) \
+		{ \
+			if(cudaMemcpy(&dstLayerRef[i].nodeMat.weight[procIndex], \
+						srcLayerRef[i].nodeList[j].base.rWeight, \
+						srcLayerRef[cfgRef->layers - 2].nodeCount * sizeof(double), \
+						cudaMemcpyHostToDevice) \
+					!= cudaSuccess) \
+			{ \
+				ret = LSTM_MEM_FAILED; \
+				goto ERR; \
+			} \
+			else \
+			{ \
+				procIndex += srcLayerRef[cfgRef->layers - 2].nodeCount; \
+			} \
+		} \
+ \
+		if(cudaMemcpy(&dstLayerRef[i].nodeMat.weight[procIndex], \
+					&srcLayerRef[i].nodeList[j].base.th, \
+					sizeof(double), \
+					cudaMemcpyHostToDevice) \
+				!= cudaSuccess) \
+		{ \
+			ret = LSTM_MEM_FAILED; \
+			goto ERR; \
+		} \
+	}
+
 	// Clone weight
 	dstLayerRef = tmpLstmCuda->layerList;
 	srcLayerRef = lstm->layerList;
 	for(i = 1; i < cfgRef->layers; i++)
 	{
-		for(j = 0; j < srcLayerRef[i].nodeCount; j++)
+		// Get node count and vector length
+		nodeCount = srcLayerRef[i].nodeCount;
+		vecLen = dstLayerRef[i].vecLen;
+
+		// Clone node to cuda
+		for(j = 0; j < nodeCount; j++)
 		{
+			// Clone gate network
 			if(i < cfgRef->layers - 1)
 			{
-				// Clone output gate network
-				// Clone forget gate network
-				// Clone input gate network
+				__lstm_clone_base_to_cuda(LSTM_CUMAT_OG, ogNet);
+				__lstm_clone_base_to_cuda(LSTM_CUMAT_FG, fgNet);
+				__lstm_clone_base_to_cuda(LSTM_CUMAT_IG, igNet);
 			}
 
 			// Clone input network
+			__lstm_clone_base_to_cuda(LSTM_CUMAT_INPUT, inputNet);
 		}
 	}
+
+	// Assign value
+	*lstmCudaPtr = tmpLstmCuda;
+
+	goto RET;
+
+ERR:
+	lstm_delete_cuda(tmpLstmCuda);
 
 RET:
 	LOG("exit");
