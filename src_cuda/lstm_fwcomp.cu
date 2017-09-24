cdfa3667dbd3c90a04f4f99ee47f2fbff704509b
Zheng-Ling Lai <jamesljlster@gmail.com>
2017-09-24 17:02:39 +0800

    Finish cuda network base calculation

--- src_cuda/lstm_fwcomp.cu
+++ src_cuda/lstm_fwcomp.cu
@@ -3,13 +3,39 @@
 
 #include <debug.h>
 #include "lstm_private_cuda.h"
+#include "lstm_builtin_math_cuda.h"
 
 void lstm_forward_computation_erase_cuda(lstm_cuda_t lstmCuda)
 {
 }
 
-__global__ void lstm_matmul(double* outBuf, double* input, double* weight)
+__global__ void lstm_matmul(double* calcBuf, double* input, double* weight, int vecLen)
 {
+	calcBuf[blockIdx.x * vecLen + threadIdx.x] =
+		weight[blockIdx.x * vecLen + threadIdx.x] * input[threadIdx.x];
+}
+
+__global__ void lstm_base_calc(double* calc, double* out, double* calcBuf, double* weight, int vecLen, int inputTFunc, int gateTFunc)
+{
+	int i;
+	int tFuncIndex;
+
+	// Copy threshold value to calcBuf
+	calcBuf[blockIdx.x * (blockDim.x * vecLen) + (threadIdx.x + 1) * vecLen - 1] =
+		weight[blockIdx.x * (blockDim.x * vecLen) + (threadIdx.x + 1) * vecLen - 1];
+
+	// Vector reduction
+	calc[blockIdx.x * blockDim.x + threadIdx.x] = 0;
+	for(i = 0; i < vecLen; i++)
+	{
+		calc[blockIdx.x * blockDim.x + threadIdx.x] +=
+			calcBuf[blockIdx.x * (blockDim.x * vecLen) + threadIdx.x * vecLen + i];
+	}
+
+	// Find network base output
+	tFuncIndex = (blockIdx.x == LSTM_CUMAT_INPUT) ? inputTFunc : gateTFunc;
+	lstm_transfer_list_cu[tFuncIndex](&out[blockIdx.x * blockDim.x + threadIdx.x],
+			calc[blockIdx.x * blockDim.x + threadIdx.x]);
 }
 
 int lstm_forward_computation_cuda(lstm_cuda_t lstmCuda, double* input, double* output)
@@ -17,6 +43,7 @@ int lstm_forward_computation_cuda(lstm_cuda_t lstmCuda, double* input, double* o
 	int i;
 	int ret = LSTM_NO_ERROR;
 	int indexTmp;
+	int vecCalcLen;
 
 	struct LSTM_CONFIG_STRUCT* cfgRef;
 	struct LSTM_CULAYER* layerRef;
@@ -28,19 +55,37 @@ int lstm_forward_computation_cuda(lstm_cuda_t lstmCuda, double* input, double* o
 	layerRef = lstmCuda->layerList;
 
 	// Copy inputs
-	cudaMemcpy(layerRef[0].out, input,
+	cudaMemcpy(layerRef[0].nodeMat.out, input,
 			cfgRef->inputs * sizeof(double),
 			cudaMemcpyHostToDevice);
 
 	// Copy recurrent output
 	indexTmp = cfgRef->layers - 2;
-	cudaMemcpy(&layerRef[0].out[cfgRef->inputs], layerRef[indexTmp].output,
+	cudaMemcpy(&layerRef[0].nodeMat.out[cfgRef->inputs], layerRef[indexTmp].nodeMat.out,
 			layerRef[indexTmp].nodeCount * sizeof(double),
 			cudaMemcpyDeviceToDevice);
 
 	// Hidden layer calculation
 	for(i = 1; i < cfgRef->layers - 1; i++)
 	{
+		// Matrix element multiplication
+		lstm_matmul<<<LSTM_CUMAT_AMOUNT * layerRef[i].nodeCount, layerRef[i].vecLen - 1>>>(
+				layerRef[i].nodeMat.calcBuf,
+				layerRef[i - 1].nodeMat.out,
+				layerRef[i].nodeMat.weight,
+				layerRef[i].vecLen);
+
+		// Network base calculation
+		lstm_base_calc<<<LSTM_CUMAT_AMOUNT, layerRef[i].nodeCount>>>(
+				layerRef[i].nodeMat.calc,
+				layerRef[i].nodeMat.out,
+				layerRef[i].nodeMat.calcBuf,
+				layerRef[i].nodeMat.weight,
+				layerRef[i].vecLen,
+				layerRef[i].inputTFunc,
+				layerRef[i].gateTFunc);
+
+		// LSTM cell calculation
 	}
 
 	// Output layer calculation
@@ -49,7 +94,7 @@ int lstm_forward_computation_cuda(lstm_cuda_t lstmCuda, double* input, double* o
 	// Get output
 	if(output != NULL)
 	{
-		cudaMemcpy(output, layerRef[indexTmp].out, cfgRef->outputs * sizeof(double));
+		cudaMemcpy(output, layerRef[indexTmp].nodeMat.out, cfgRef->outputs * sizeof(double), cudaMemcpyDeviceToHost);
 	}
 
 RET:
