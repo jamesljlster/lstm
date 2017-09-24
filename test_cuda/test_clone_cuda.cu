d57986442186ebc51687d6e9de2a00fef0b1bd81
Zheng-Ling Lai <jamesljlster@gmail.com>
2017-09-24 11:00:07 +0800

    Add clone validation in clone test

--- test_cuda/test_clone_cuda.cu
+++ test_cuda/test_clone_cuda.cu
@@ -10,6 +10,10 @@ int main(int argc, char* argv[])
 
 	cudaError_t cuErr;
 
+	struct LSTM_LAYER* srcLayerRef;
+	struct LSTM_LAYER* cmpLayerRef;
+	struct LSTM_CONFIG_STRUCT* cfgRef;
+
 	lstm_t srcLstm = NULL;
 	lstm_t cmpLstm = NULL;
 	lstm_cuda_t cuLstm = NULL;
@@ -48,14 +52,63 @@ int main(int argc, char* argv[])
 		goto RET;
 	}
 
+#define __lstm_clone_validation(base) \
+	iResult = memcmp(srcLayerRef[i].nodeList[j].base.weight, \
+			cmpLayerRef[i].nodeList[j].base.weight, \
+			srcLayerRef[i - 1].nodeCount * sizeof(double)); \
+	if(iResult != 0) \
+	{ \
+		printf("Clone validation failed!\n"); \
+		ret = -1; \
+		goto RET; \
+	} \
+\
+	if(i == 1) \
+	{ \
+		iResult = memcmp(srcLayerRef[i].nodeList[j].base.rWeight, \
+				cmpLayerRef[i].nodeList[j].base.rWeight, \
+				srcLayerRef[cfgRef->layers - 2].nodeCount * sizeof(double)); \
+		if(iResult != 0) \
+		{ \
+			printf("Clone validation failed!\n"); \
+			ret = -1; \
+			goto RET; \
+		} \
+	} \
+\
+	if(srcLayerRef[i].nodeList[j].base.th != cmpLayerRef[i].nodeList[j].base.th) \
+	{ \
+		printf("Clone validation failed!\n"); \
+		ret = -1; \
+		goto RET; \
+	} \
+
+	// Verify data
+	srcLayerRef = srcLstm->layerList;
+	cmpLayerRef = cmpLstm->layerList;
+	cfgRef = &srcLstm->config;
+	for(i = 1; i < cfgRef->layers; i++)
+	{
+		for(j = 0; j < srcLayerRef[i].nodeCount; j++)
+		{
+			if(i < cfgRef->layers - 1)
+			{
+				__lstm_clone_validation(ogNet);
+				__lstm_clone_validation(igNet);
+				__lstm_clone_validation(fgNet);
+			}
+
+			__lstm_clone_validation(inputNet);
+		}
+	}
+
 RET:
 	cuErr = cudaGetLastError();
 	printf("Last cuda error: %s, %s\n", cudaGetErrorName(cuErr), cudaGetErrorString(cuErr));
-	/*
+
 	lstm_delete(srcLstm);
 	lstm_delete(cmpLstm);
 	lstm_delete_cuda(cuLstm);
-	*/
 
 	return 0;
 }
